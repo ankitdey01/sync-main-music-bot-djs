7737ff25b7109dc4fe87874ac949a7c3a7eeb869
Ankit Dey <111570617+ankitdey01@users.noreply.github.com>
2026-03-18 16:34:02 +0530

    Remove unused Lavalink EC2 node configuration
    Removed commented-out configuration for Lavalink EC2.

--- src/systems/nodes.ts
+++ src/systems/nodes.ts
@@ -1,12 +1,6 @@
 import { NodeOption } from "shoukaku";
 
 const nodes: NodeOption[] = [
-    // {
-    //     name: "Lavalink EC2",
-    //     url: "13.234.226.165:2333",
-    //     auth: "ankit123#",
-    //     secure: false
-    // },
     {
         name: "Lavalink Local",
         url: "127.0.0.1:2333",
